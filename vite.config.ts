3177658e799dc8d05b2eafef32b0fca8185c0f85
niklaus699 <nicholasbasseyy@gmail.com>
2026-04-09 03:57:31 +0100

    Fix: Added Buffer polyfills for Web3 support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), // This adds Buffer and Process support to the browser
+    nodePolyfills({
+      globals: {
+        Buffer: true, 
+        process: true,
+      },
+    }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
