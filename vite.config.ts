a7cf77008322051b41bbe32d6e6544ef1c740c85
niklaus699 <nicholasbasseyy@gmail.com>
2026-04-09 04:11:54 +0100

    Brute force polyfills and fix env syntax

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,11 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), // This adds Buffer and Process support to the browser
-    nodePolyfills({
-      globals: {
-        Buffer: true, 
-        process: true,
-      },
-    }), tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  define: {global: 'window'},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
